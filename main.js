3b38b5c8f2fceb308384340f4345281ec906ae4d
JulianaHansen <juliana.hansen@estudante.ufjf.br>
2025-08-19 23:00:35 -0300

    tabuleiro

--- main.js
+++ main.js
@@ -4,8 +4,27 @@ const message = document.getElementById('message');
 
 function updateBoard() {
     board.innerHTML = '';
-    // 
-    checkWin();
+    
+    for (let i = 0; i < 7; i++) {
+        for (let j = 0; j < 7; j++) {
+            const cell = document.createElement('div');
+            cell.classList.add('cell');
+
+            if ((i < 2 || i > 4) && (j < 2 || j > 4)) {
+                cell.classList.add('invalid');
+            } else {
+                const circle = document.createElement('div');
+                circle.classList.add('circle');
+                if (!(i === 3 && j === 3)) {
+                    circle.classList.add('piece');
+                }
+                cell.appendChild(circle);
+            }
+            board.appendChild(cell);
+        }
+    }
+
+    //checkWin();
 }
 
 function handleClick(event) {
